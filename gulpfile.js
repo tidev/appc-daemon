39030830cfcb660c499986b08ed1b4f234c2abc8
Chris Barber <chris@cb1inc.com>
2016-10-25 18:33:22 -0500

    Added install task

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,63 @@ const gulp = require('gulp');
 const path = require('path');
 const runSequence = require('run-sequence');
 const spawn = require('child_process').spawn;
-const yarn = require('yarn');
+
+function runYarn(dir) {
+	return new Promise((resolve, reject) => {
+		const child = spawn(
+			process.execPath,
+			[ path.resolve(__dirname, 'node_modules', 'yarn', 'bin', 'yarn.js') ],
+			{
+				cwd: path.resolve(__dirname, dir),
+				stdio: 'inherit'
+			}
+		);
+		child.on('close', code => {
+			// always resolve?
+			resolve();
+		});
+	});
+}
 
 /*
- * install task
+ * install tasks
  */
 gulp.task('install', () => {
+	return runSequence('install-deps', 'build');
+});
+
+gulp.task('install-deps', callback => {
+	const dirs = ['bootstrap', 'client', 'core'];
+	const pluginsDir = path.join(__dirname, 'plugins');
+	fs.readdirSync(pluginsDir).forEach(dir => {
+		try {
+			if (fs.statSync(path.join(pluginsDir, dir, 'package.json'))) {
+				dirs.push(dir);
+			}
+		} catch (e) {
+			// squeltch
+		}
+	});
+
+	Promise
+		.all(dirs.map(dir => runYarn(dir)))
+		.then(() => callback(), callback);
 });
 
 /*
  * build tasks
  */
-gulp.task('build', ['build-bootstrap', 'build-core']); //, 'build-plugins']);
+gulp.task('build', ['build-bootstrap', 'build-client', 'build-core', 'build-plugins']);
 
 gulp.task('build-bootstrap', () => {
 	return gulp
 		.src(__dirname + '/bootstrap/gulpfile.js')
+		.pipe(chug({ tasks: ['build'] }));
+});
+
+gulp.task('build-client', () => {
+	return gulp
+		.src(__dirname + '/client/gulpfile.js')
 		.pipe(chug({ tasks: ['build'] }));
 });
 
@@ -41,7 +82,7 @@ gulp.task('build-plugins', () => {
  */
 let children = 0;
 gulp.task('restart-daemon', () => {
-	const child = spawn(process.execPath, ['bin/appcd', 'restart', '--debug'], { stdio: 'inherit' });
+	const child = spawn(process.execPath, ['bootstrap/bin/appcd', 'restart', '--debug'], { stdio: 'inherit' });
 	children++;
 	child.on('exit', () => {
 		// if appcd is killed via kill(1), then we force gulp watch to exit
@@ -55,6 +96,9 @@ gulp.task('watch', () => {
 	runSequence('build', 'restart-daemon', () => {
 		gulp.watch(__dirname + '/bootstrap/src/**/*.js', () => {
 			runSequence('build-bootstrap', 'restart-daemon');
+		});
+		gulp.watch(__dirname + '/client/src/**/*.js', () => {
+			runSequence('build-client', 'restart-daemon');
 		});
 		gulp.watch(__dirname + '/core/src/**/*.js', () => {
 			runSequence('build-core', 'restart-daemon');
