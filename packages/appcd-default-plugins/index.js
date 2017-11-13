0ed71572ab870f0711a398e361171d6d615330da
Chris Barber <chris@cb1inc.com>
2017-11-13 08:05:24 -0600

    Fixed default plugin resolver.

--- packages/appcd-default-plugins/index.js
+++ packages/appcd-default-plugins/index.js
@@ -2,29 +2,33 @@ const fs = require('fs');
 const path = require('path');
 const { dependencies } = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
 const deps = new Set(Object.keys(dependencies).filter(n => n.startsWith('appcd-plugin-')));
-const dirs = path.resolve(__dirname).split(path.sep);
+let modulesDir = __dirname;
+let lastDir = null;
 
 module.exports = [];
 
-while (dirs.length) {
+function isDir(dir) {
+	try {
+		return fs.statSync(dir).isDirectory();
+	} catch (e) {
+		return false;
+	}
+}
+
+while (modulesDir !== lastDir) {
 	if (!deps.size) {
 		// all done
 		break;
 	}
 
-	const modulesDir = path.resolve(...dirs, 'node_modules');
-
 	for (const dep of deps) {
-		try {
-			const depDir = path.join(modulesDir, dep);
-			if (fs.statSync(depDir).isDirectory()) {
-				module.exports.push(depDir);
-				deps.delete(dep);
-			}
-		} catch (e) {
-			// squelch
+		let depDir = path.join(modulesDir, 'node_modules', dep);
+		if (isDir(depDir) || isDir(depDir = path.join(modulesDir, dep))) {
+			module.exports.push(depDir);
+			deps.delete(dep);
 		}
 	}
 
-	dirs.pop();
+	lastDir = modulesDir;
+	modulesDir = path.dirname(modulesDir);
 }
