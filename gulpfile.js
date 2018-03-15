a4183ec50e5965c07a5341ac9881fd8587de3e9a
Chris Barber <chris@cb1inc.com>
2018-03-15 00:15:00 -0500

    Updated to babel scope packages.

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ const cliTableChars = {
 	top: '', 'top-left': '', 'top-mid': '', 'top-right': ''
 };
 
-const dontUpdate = [ 'gulp-babel' ];
+const dontUpdate = [];
 
 const appcdRE = /^appcd-/;
 const appcdPackages = new Set(fs.readdirSync(path.join(__dirname, 'packages')).filter(name => appcdRE.test(name)));
