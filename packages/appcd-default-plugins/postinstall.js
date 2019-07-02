7e377cec0e7d4c6c1298b503bb982bcdacd0e051
Chris Barber <chris@cb1inc.com>
2019-07-02 10:32:20 -0500

    fix: Wrapped the entire postinstall script in a try/catch since it could fail if gulp build hasn't been run when working from the monorepo.

--- packages/appcd-default-plugins/postinstall.js
+++ packages/appcd-default-plugins/postinstall.js
@@ -1,24 +1,26 @@
-const os = require('os');
-const { createInstanceWithDefaults, StdioStream } = require('snooplogg');
-const { installDefaultPlugins } = require('./dist/index');
+try {
+	const os = require('os');
+	const { createInstanceWithDefaults, StdioStream } = require('snooplogg');
+	const { installDefaultPlugins } = require('./dist/index');
 
-const logger = createInstanceWithDefaults()
-	.snoop()
-	.config({
-		maxBufferSize: 1000,
-		minBrightness: 80,
-		maxBrightness: 200
-	})
-	.enable('*')
-	.pipe(new StdioStream(), { flush: true })
-	.ns('appcd:default-plugins:postinstall');
+	const logger = createInstanceWithDefaults()
+		.snoop()
+		.config({
+			maxBufferSize: 1000,
+			minBrightness: 80,
+			maxBrightness: 200
+		})
+		.enable('*')
+		.pipe(new StdioStream(), { flush: true })
+		.ns('appcd:default-plugins:postinstall');
 
-installDefaultPlugins(`${os.homedir()}/.appcelerator/appcd/plugins`)
-	.catch(err => {
-		if (err.code === 'EACCES') {
-			logger.warn(err);
-		} else {
-			logger.error(err);
-			process.exit(1);
-		}
-	});
+	installDefaultPlugins(`${os.homedir()}/.appcelerator/appcd/plugins`)
+		.catch(err => {
+			if (err.code === 'EACCES') {
+				logger.warn(err);
+			} else {
+				logger.error(err);
+				process.exit(1);
+			}
+		});
+} catch (e) {}
