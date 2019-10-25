aa72c4868ae943fec87c4d07038aba3b06204d15
Jan Vennemann <jan.vennemann@gmx.net>
2019-10-25 12:48:11 +0200

    test: add require api tests

---
+++ packages/appcd-plugin/test/fixtures/require-api.js
@@ -0,0 +1,5 @@
+exports.main = require.main;
+
+exports.resolved = require.resolve('fs-extra');
+
+exports.paths = require.resolve.paths('fs-extra');
--- packages/appcd-plugin/test/test-plugin-module.js
+++ packages/appcd-plugin/test/test-plugin-module.js
@@ -6,35 +6,51 @@ function MockPlugin() {
 	this.globals = {};
 }
 
-describe('Plugin Module', () => {
+describe.only('Plugin Module', () => {
+	let plugin;
+
+	beforeEach(() => {
+		plugin = new MockPlugin();
+	});
+
+	afterEach(() => {
+		plugin = null;
+	});
+
 	it('should load a vanilla js file', () => {
-		const plugin = new MockPlugin();
 		const exports = PluginModule.load(plugin, path.join(__dirname, 'fixtures', 'vanilla.js'));
 		expect(exports).to.deep.equal({ foo: 'bar' });
 	});
 
 	it('should load a js file that requires a vanilla js file', () => {
-		const plugin = new MockPlugin();
 		const exports = PluginModule.load(plugin, path.join(__dirname, 'fixtures', 'req.js'));
 		expect(exports).to.deep.equal({ foo: 'bar' });
 	});
 
 	it('should fail to load if js file requires a non-existent file', () => {
-		const plugin = new MockPlugin();
 		expect(() => {
 			PluginModule.load(plugin, path.join(__dirname, 'fixtures', 'bad-req.js'));
 		}).to.throw(Error, 'Failed to load plugin: Cannot find module \'does_not_exist\'');
 	});
 
 	it('should load js file that requires built-in module', () => {
-		const plugin = new MockPlugin();
 		const exports = PluginModule.load(plugin, path.join(__dirname, 'fixtures', 'req-builtin.js'));
 		expect(exports).to.equal(os.hostname());
 	});
 
 	it('should load js file that requires a 3rd party module', () => {
-		const plugin = new MockPlugin();
 		const exports = PluginModule.load(plugin, path.join(__dirname, 'fixtures', 'req-3rd-party.js'));
 		expect(exports).to.equal(os.hostname());
+	});
+
+	it('should provide Node.js compatible require api', () => {
+		const testModule = path.join(__dirname, 'fixtures', 'require-api.js');
+		const exports = PluginModule.load(plugin, testModule);
+		expect(exports.main).to.equal(process.mainModule);
+		expect(exports.resolved).to.equal(require.resolve('fs-extra'));
+		const paths = require.resolve.paths('fs-extra');
+		// manually add fixtures subpath
+		paths.unshift(path.join(path.dirname(testModule), 'node_modules'));
+		expect(exports.paths).to.eql(paths);
 	});
 });
