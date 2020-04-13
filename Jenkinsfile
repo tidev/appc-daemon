1ac0a156c518b42f99c6292ec1948997462066c1
Chris Barber <chris@cb1inc.com>
2020-04-13 12:50:35 -0500

    Moved plugin linking to gulp task.

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,12 +3,11 @@ library 'pipeline-library'
 
 runNPMPackage {
   defaultNodeJSVersion = '12.16.2' // keep this in sync with appcd-core Node.js version!
-  nodeVersions = [ '10.19.0', '12.16.2', '13.11.0' ]
+  nodeVersions = [ '10.19.0' ] // , '12.16.2', '13.11.0' ]
   packageJsonPath = 'packages/appcd/package.json'
-  platforms = [ 'linux', 'osx' ]
+  platforms = [ 'linux' ] // , 'osx' ]
   publish = false
   securityCommands = [ 'yarn run gulp check' ]
   successThreshold = 1
-  testPostInstallCommands = [ 'yarn run gulp build', 'node packages/appcd/bin/appcd pm install default' ]
   useYarn = true
 }
--- gulpfile.js
+++ gulpfile.js
@@ -365,6 +365,9 @@ async function runTests(cover, all) {
 			process.env.HOMEPATH = tmpHomeDir.replace(process.env.HOMEDRIVE, '');
 		}
 
+		log(`Linking default plugins...`);
+		spawnSync(process.execPath, [ 'packages/appcd/bin/appcd', 'pm', 'link' ], { stdio: 'inherit' });
+
 		require('./packages/appcd-gulp/src/test-runner').runTests({ root: __dirname, projectDir: __dirname, cover, all });
 	} finally {
 		// restore home directory so that we can delete the temp one
@@ -382,7 +385,7 @@ exports['integration-test']      = series(nodeInfo, build, function test()     {
 exports['integration-test-only'] = series(nodeInfo,        function test()     { return runTests(); });
 exports['integration-coverage']  = series(nodeInfo,        function coverage() { return runTests(true); });
 exports['coverage']              = series(nodeInfo,        function coverage() { return runTests(true, true); });
-exports['coverage-ci']           = series(nodeInfo, build, function coverage() { return runTests(true, true); });
+exports['coverage-ci']           = series(nodeInfo, build, linkPlugins, function coverage() { return runTests(true, true); });
 
 /*
  * watch/debug tasks
